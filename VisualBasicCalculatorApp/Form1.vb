8947692d0e5f05c48d7267c08fdd18ae0289a9e1
Jake M <jm05259722@priestley.ac.uk>
2024-03-08 15:14:14 +0000

    Add project files.

---
+++ VisualBasicCalculatorApp/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim varNumber1 As Integer
+        Dim varNumber2 As Integer
+
+        varNumber1 = TextBox1.Text
+        varNumber2 = TextBox2.Text
+
+        TextBox3.Text = varNumber1 + varNumber2
+
+    End Sub
+
+End Class
